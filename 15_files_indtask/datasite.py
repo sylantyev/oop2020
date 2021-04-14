409b228040424c1bc95e1a6e1c87a0db9ea00afa
Sergiy Sylantyev <sylantyev@nubip.edu.ua>
2021-04-14 22:07:02 +0300

    date from site

---
+++ 15_files_indtask/datasite.py
@@ -0,0 +1,102 @@
+question_data = [
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "5-ий Стандарт мови розмітки гіпертексту  HTML5 був оприлюднений у 2014 році",
+        "correct_answer": "Y",
+        "incorrect_answers": [
+            "N"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Перша помилка комп'ютера була ініційована несправними проводами",
+        "correct_answer": "N",
+        "incorrect_answers": [
+            "Y"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "FLAC є скороченою назвою 'вільний аудіокодек без втрат'.",
+        "correct_answer": "Y",
+        "incorrect_answers": [
+            "N"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "В системі Windows коди програм можуть бути скомпілованими в *.EXE файли. Потім, цей файл може бути виконаний на будь-якої машині",
+        "correct_answer": "N",
+        "incorrect_answers": [
+            "Y"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Лінус Торвальдс створив Linux і Git.",
+        "correct_answer": "Y",
+        "incorrect_answers": [
+            "N"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Мова програмування 'Python' базується на модифікованій версії 'JavaScript'",
+        "correct_answer": "Y",
+        "incorrect_answers": [
+            "N"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Компанія AMD першою розробила 64-bit процесор для ринку.",
+        "correct_answer": "Y",
+        "incorrect_answers": [
+            "N"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "'HTML' можна перекласти як мова розмітки гіпертексту.",
+        "correct_answer": "Y",
+        "incorrect_answers": [
+            "N"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "У більшості мов програмування оператор ++ еквівалентний оператору '+= 1'.",
+        "correct_answer": "Y",
+        "incorrect_answers": [
+            "N"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "ПК IBM PC використовував мікропроцесор Intel 8008 з тактовою частотою 4,77 МГц та 8 кілобайтами пам'яті.",
+        "correct_answer": "N",
+        "incorrect_answers": [
+            "Y"
+        ]
+    }
+]
